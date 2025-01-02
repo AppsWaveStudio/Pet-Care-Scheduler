94c05d74d9de3bf42c371e286ab868d72d82279a
appswavestudio <appswave.studio@gmail.com>
2025-01-02 20:26:00 +0500

    update

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+const String API = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkeXR2ZnBubW94bGVrZ2xyaHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MzEwNzksImV4cCI6MjA1MTQwNzA3OX0.JZBekQ1KIKMTT5odE8g9XPTdYRZbsqKZScFAHW37eKs';
+const String url = 'https://fdytvfpnmoxlekglrhxk.supabase.co';
